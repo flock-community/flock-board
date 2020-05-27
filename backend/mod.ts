b5de29521c1afc433ebdea4542f2e5d9539d2268
Kasper Peulen <kpeulen@bol.com>
2020-05-27 13:54:05 +0200

    test kasper

--- backend/mod.ts
+++ backend/mod.ts
@@ -25,7 +25,7 @@ for await (const request of server) {
     } else if (request.url === '/project') {
       request.respond({ body: 'Projects\n' });
     } else if (request.url === '/test') {
-      request.respond({ body: 'test 123\n' });
+      request.respond({ body: 'test kasper\n' });
     } else {
       request.respond({ body: 'Hello World\n' });
     }
