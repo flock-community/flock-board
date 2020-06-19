74f38ae8b17acba7705f00f4704ac448218b5d82
Vincent de Bruijn <vincent.de.bruijn@flock-se.com>
2020-06-19 14:00:26 +0200

    Fix after review comment

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import "./App.css";
 import { AppRouter } from "./AppRouter";
 
-function App() {
+export function App() {
   return <AppRouter></AppRouter>;
 }
-
-export default App;
