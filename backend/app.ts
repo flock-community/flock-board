5b190dc411cb337d9d890c81193bb00d8564e94e
Kasper Peulen <kpeulen@bol.com>
2020-06-19 14:48:19 +0200

    proxy to localhost:3000 for development

---
+++ backend/app.ts
@@ -0,0 +1,20 @@
+import {
+  StaticServer,
+  StaticServerDevelopment,
+  StaticServerProduction,
+} from './services/static-server.ts';
+
+interface App {
+  staticServer: StaticServer;
+}
+
+export let app: App;
+
+export function createApp() {
+  const profiles = Deno.env.get('PROFILE')?.split(',') ?? [];
+  const dev = profiles.includes('development');
+
+  app = {
+    staticServer: dev ? new StaticServerDevelopment() : new StaticServerProduction(),
+  };
+}
--- frontend/src/clients/project-client.ts
+++ frontend/src/clients/project-client.ts
@@ -1,22 +1,22 @@
 import { Project } from '../../model/graphql/TypeScript/board';
 
 export function getProjects(): Promise<Project[]> {
-  // return fetch('/projects', {
-  //   method: 'GET',
-  //   headers: {
-  //     Accept: 'application/json',
-  //   },
-  // }).then((it) => it.json() as Promise<Project[]>);
-  //
-  return Promise.resolve([
-    {
-      id: 'id',
-      name: 'Project',
-      description: 'project description',
-      timestamp: new Date(),
-      state: 'OPEN',
+  return fetch('/projects', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
     },
-  ]);
+  }).then((it) => it.json() as Promise<Project[]>);
+
+  // return Promise.resolve([
+  //   {
+  //     id: 'id',
+  //     name: 'Project',
+  //     description: 'project description',
+  //     timestamp: new Date(),
+  //     state: 'OPEN',
+  //   },
+  // ]);
 }
 
 export function postProject(project: Project): Promise<any> {
