58845bfe1d85638bf661e20dcaa171e5f3c6ed65
Vincent de Bruijn <vincent.de.bruijn@flock-se.com>
2020-07-17 16:12:55 +0200

    Edit and delete projects

--- frontend/src/clients/project-client.ts
+++ frontend/src/clients/project-client.ts
@@ -1,4 +1,4 @@
-import type { Project } from "../../target/model/board";
+import { Project } from "../../target/model/board";
 import * as z from "myzod";
 import { projectSchema } from "../model/project";
 
@@ -21,5 +21,37 @@ export function postProject(project: Project) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(project),
+  });
+}
+
+export function getProject(id: String): Promise<Project> {
+  return fetch(`/api/project/${id}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((it) => it.json())
+    .then((json) => projectSchema.parse(json));
+}
+
+export function updateProject(project: Project) {
+  return fetch("/api/project", {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(project),
+  });
+}
+
+export function deleteProject(project: Project) {
+  return fetch("/api/project", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id: project.id }),
   });
 }
