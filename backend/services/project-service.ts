8cff654153e747d7f7ab94fa7bf26c236760585c
Kasper Peulen <kpeulen@bol.com>
2020-07-17 10:57:14 +0200

    use all fields in project-service.ts

--- backend/services/project-service.ts
+++ backend/services/project-service.ts
@@ -1,39 +1,33 @@
-import {CrudService} from "./crud-service.ts";
-import {Project} from "../../frontend/model/graphql/TypeScript/board.ts";
-import {Project as ProjectDb} from "../../database/mod.ts"
+import { CrudService } from './crud-service.ts';
+import { Project } from '../../frontend/model/graphql/TypeScript/board.ts';
+import { Project as ProjectDb } from '../../database/mod.ts';
 
 export class ProjectService implements CrudService<Project> {
-    async create(project: Project): Promise<Project> {
-        return ProjectDb.create({
-            name: project.name,
-            description: project.description,
+  async create(project: Project): Promise<Project> {
+    return ProjectDb.create({ ...project });
+  }
 
-        })
-    }
+  delete(id: Project['id']): Promise<void> {
+    return ProjectDb.deleteById(id);
+  }
 
-    delete(id: Project["id"]): Promise<void> {
-        return ProjectDb.deleteById(id);
-    }
+  get(id: Project['id']): Promise<Project> {
+    return ProjectDb.find(id);
+  }
 
-    get(id: Project["id"]): Promise<Project> {
-        return ProjectDb.find(id);
-    }
+  getAll(): Promise<Project[]> {
+    return ProjectDb.all();
+  }
 
-    getAll(): Promise<Project[]> {
-        return ProjectDb.all();
-    }
+  async update(id: Project['id'], mutation: Partial<Project>): Promise<Project> {
+    const project = await ProjectDb.find(id);
+    return ProjectDb.update(id, {
+      ...project,
+      ...mutation,
+    });
+  }
 
-    async update(id: Project["id"], mutation: Partial<Project>): Promise<Project> {
-        const project = await ProjectDb.find(id);
-        return ProjectDb.update(id, {
-            ...project,
-            ...mutation
-        })
-    }
-
-    async updateAll(entities: Project[]): Promise<void> {
-        await Promise.all(entities
-            .map(it => this.update(it.id, it)))
-    }
-
+  async updateAll(entities: Project[]): Promise<void> {
+    await Promise.all(entities.map((it) => this.update(it.id, it)));
+  }
 }
